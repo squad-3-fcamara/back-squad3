bbf8216fc6e9128d539ce774d54712ad4154ecdc
Rafael Dias <rafaelldiias100@gmail.com>
2022-11-07 14:45:39 -0300

    feat: middleware para proteger as rotas que o usuario precisa estar logado

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { cadastrarUsuario, logarUsuario } = require("./controllers/usuario");
+const verificarUsuarioLogado = require("./middlewares/verificarUsuarioLogado");
+
 const routes = express();
 
 routes.post("/usuario/cadastro", cadastrarUsuario);
 routes.post("/usuario/login", logarUsuario);
+
+routes.use(verificarUsuarioLogado);
 
 module.exports = routes;
