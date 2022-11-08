99f392e34837870bc45ab8cafb6a24cd0f442bfc
Rafael Dias <rafaelldiias100@gmail.com>
2022-11-08 18:40:01 -0300

    fix: colocado nome da váriavel de trilhas no plural

--- src/validations/verificarTrilha.js
+++ src/validations/verificarTrilha.js
@@ -1,12 +1,12 @@
-function verificarTrilha(trilha) {
-  if (!trilha) {
+function verificarTrilha(trilhas) {
+  if (!trilhas) {
     return "Você deve informar a trilha.";
   }
-  if (trilha.length > 3) {
+  if (trilhas.length > 3) {
     return "Você está informando mais trilhas que o permitido.";
   }
-  for (let t of trilha) {
-    if (t !== "full stack" && t !== "ux" && t !== "qa") {
+  for (let trilha of trilhas) {
+    if (trilha !== "full stack" && trilha !== "ux" && trilha !== "qa") {
       return "Você está informando trilha que não existe.";
     }
   }
