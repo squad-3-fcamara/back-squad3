92f4b818dc8c76ad6e7645e01c9bcee0c158413c
Rafael Dias <rafaelldiias100@gmail.com>
2022-11-09 15:04:33 -0300

    fix: adicionado validação da trilha

--- src/controllers/trilhas.js
+++ src/controllers/trilhas.js
@@ -1,5 +1,6 @@
 const knex = require("../conexao");
 const cadastrarTrilha = require("../utils/cadastrarTrilha");
+const verificarTrilha = require("../validations/verificarTrilha");
 
 const listarTrilhas = async (req, res) => {
   const trilhas = await knex("trilhas").select("*");
@@ -9,6 +10,11 @@ const listarTrilhas = async (req, res) => {
 const alterarTrilhas = async (req, res) => {
   const usuario = req.usuario;
   const { trilhas } = req.body;
+
+  const erroTrilha = await verificarTrilha(trilhas);
+  if (erroTrilha) {
+    return res.status(400).json(erroTrilha);
+  }
 
   await knex("inscricoes").del().where("id_usuario", usuario.id);
   for (let trilha of trilhas) {
